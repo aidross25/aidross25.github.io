8ea88fdb9172b61227ba9b36561adca892dbc5bf
Aiden Ross <aidenmross@gmail.com>
2024-12-08 12:46:05 -0500

    text

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ const titles = ["a Software Developer", "a Student", "Driven", "Passionate"];
 let titleIndex = 0;
 
 function updateTitle() {
-    document.getElementById("dynamic-title").textContent = titles[titleIndex];
+    document.getElementById("dynamic-text").textContent = titles[titleIndex];
     titleIndex = (titleIndex + 1) % titles.length;
 }
 
